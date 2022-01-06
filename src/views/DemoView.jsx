7314be84705d23bd38ce5be678581e6e4c5be07d
Michael Reynolds <michael4reynolds@gmail.com>
2022-01-06 02:50:52 -0500

    remove Demo component for now

--- src/views/DemoView.jsx
+++ src/views/DemoView.jsx
@@ -1,9 +1,7 @@
-import Demo from '@michael4reynolds/component-library/src/components';
 import React from 'react';
 
 export const DemoView = () => (
   <div>
     <h1>This is your brand new application scaffold!</h1>
-    <Demo />
   </div>
 );
